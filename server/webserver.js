da2ea2be9f4b391a52255b4022b639e07d7edac4
David Ludwig <davidludwigii@gmail.com>
2020-01-28 22:31:16 -0600

    Install Mustache template engine

--- server/webserver.js
+++ server/webserver.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const path = require("path");
+const mustacheExpress = require("mustache-express");
 const app = express();
 
 module.exports = {
 	serve(port) {
+
+		/**
+		 * Use the Mustache template engine
+		 */
+		app.engine("mustache", mustacheExpress());
+
+		app.set("view engine", "mustache");
+		app.set("views", __dirname + "/../resources/views");
+
+		/**
+		 * Allow fetching static resources
+		 */
 		app.use(express.static(path.join(`${__dirname}/../public`)));
 
+		// Routing ---------------------------------------------------------------------------------
+
+		/**
+		 * Route to the game page
+		 */
 		app.get('/', function (req, res) {
 			res.sendFile(path.join(`${__dirname}/../public/index.htm`));
 		});
 
-		app.get('/.*', function (req, res) {
-			res.send(req);
-			// res.sendFile(path.join(`${__dirname}/../public/`))
-		});
-
+		/**
+		 * Listen on the specified port
+		 */
 		app.listen(port);
 	}
 };
